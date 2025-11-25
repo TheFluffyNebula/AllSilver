ba66a86a8e75c230c4b9270ea63323d491d06e92
Jason Yin <jyin23@ncsu.edu>
2025-11-24 21:51:49 -0500

    February 2013 Q3

--- 2013-02/msched.py
+++ 2013-02/msched.py
@@ -1,8 +1,40 @@
 import sys
 sys.stdin = open("msched.in")
-# sys.stdout = open("msched.out", 'w')
+sys.stdout = open("msched.out", 'w')
+from collections import deque
 
+n, m = map(int, input().split())
+times = [0] * n
+for i in range(n):
+    times[i] = int(input())
+# print(times)
+indegree = [0] * n
+adj = {i: [] for i in range(n)}
+for i in range(m):
+    a, b = map(int, input().split())
+    a -= 1
+    b -= 1
+    adj[a].append(b)
+    indegree[b] += 1
 
+# base distance, these will go up
+dist = [times[i] for i in range(n)]
+# those that have no dependencies start
+q = deque()
+for i in range(n):
+    if indegree[i] == 0:
+        q.append(i)
+# bfs
+while q:
+    u = q.popleft()
+    for v in adj[u]:
+        indegree[v] -= 1
+        dist[v] = max(dist[v], dist[u] + times[v])
+        if indegree[v] == 0:
+            # all previous cows are done!
+            q.append(v)
+
+print(max(dist))
 
 '''
 some ideas:
