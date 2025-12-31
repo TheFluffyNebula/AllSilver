cb37dbbfec2bb3425b6f3339f8f8223e3628eddd
Jason Yin <jyin23@ncsu.edu>
2025-12-31 12:10:09 -0500

    December 2014 Q3

--- 2014-12/cowjog.py
+++ 2014-12/cowjog.py
@@ -1,10 +1,20 @@
 import sys
 sys.stdin = open("cowjog.in")
-# sys.stdout = open("cowjog.out", 'w')
+sys.stdout = open("cowjog.out", 'w')
 
 n, t = map(int, input().split())
 cows = [tuple(map(int, input().split())) for _ in range(n)]
 cows.sort()
+# print(cows)
+
+ans = 0
+cur = float('inf')
+for i in range(n - 1, -1, -1):
+    furthest = cows[i][0] + t * cows[i][1]
+    if furthest < cur:
+        cur = furthest
+        ans += 1
+print(ans)
 
 '''
 could tackle this problem by iterating in reverse order
