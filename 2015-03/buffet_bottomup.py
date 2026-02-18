a4d62881fabe6eaa8a50d74ae415e488fb2f320b
Jason Yin <jyin23@ncsu.edu>
2026-02-17 22:47:16 -0500

    March 2015 Q3, Clean AK!

---
+++ 2015-03/buffet_bottomup.py
@@ -0,0 +1,65 @@
+import sys
+sys.stdin = open("buffet.in")
+sys.stdout = open("buffet.out", 'w')
+input = sys.stdin.readline
+from collections import deque
+
+'''
+what if we create a distances table via bfs from every node
+now, dist[source][dest] is O(1)
+from there, start at each one and go to others
+sounds like a O(n^2) DP
+'''
+
+n, e = map(int, input().split())
+adj = {i: [] for i in range(n)}
+quality = [0] * n
+edges = set()
+for a in range(n):
+    nums = list(map(int, input().split()))
+    q = nums[0]
+    quality[a] = q
+    connections = nums[1]
+    for j in range(connections):
+        idx = 2 + j
+        b = nums[idx] - 1
+        if (min(a, b), max(a, b)) not in edges:
+            adj[a].append(b)
+            adj[b].append(a)
+            edges.add((min(a, b), max(a, b)))
+# print(adj)
+
+def bfs(start):
+    q = deque()
+    dist[start][start] = 0
+    q.append((start, 0))
+    while q:
+        u, uCost = q.popleft()
+        for v in adj[u]:
+            alt = uCost + 1
+            if alt < dist[start][v]:
+                dist[start][v] = alt
+                q.append((v, alt))
+
+dist = [[float('inf') for _ in range(n)] for _ in range(n)]
+for i in range(n):
+    bfs(i)
+# print(dist)
+
+# define the order of which we can traverse
+# (quality, index)
+qualities = list(zip(quality, [i for i in range(n)]))
+qualities.sort()
+# print(qualities)
+
+dp = [0] * n
+for i in range(n):
+    dp[i] = qualities[i][0]
+
+for i in range(n):
+    for j in range(i + 1, n):
+        # jump from i to j
+        if qualities[i][0] == qualities[j][0]:
+            continue
+        dp[j] = max(dp[j], dp[i] + qualities[j][0] - e * dist[qualities[i][1]][qualities[j][1]])
+print(max(dp))
